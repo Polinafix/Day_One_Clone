c89770d7e3ffd6614e7ab384a81b371638e5f111
Polina Fiksson <pd.fiksson@gmail.com>
2018-05-23 13:08:30 -0500

    Change keyboard height

--- Day One Clone/Controller/CreateJournalViewController.swift
+++ Day One Clone/Controller/CreateJournalViewController.swift
@@ -10,26 +10,56 @@ import UIKit
 
 class CreateJournalViewController: UIViewController {
 
+    @IBOutlet weak var bottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var navBar: UINavigationBar!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    @IBOutlet weak var setDatabutton: UIButton!
+    @IBOutlet weak var stackView: UIStackView!
+    @IBOutlet weak var aboveNavBarView: UIView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        navBar.barTintColor = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.00)//4cc1fc
+        aboveNavBarView.backgroundColor = UIColor(red: 0.298, green: 0.757, blue: 0.988, alpha: 1.00)
+        navBar.tintColor = .white
+        navBar.isTranslucent = false
+        navBar.titleTextAttributes = [.foregroundColor:UIColor.white]
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: Notification.Name.UIKeyboardWillHide, object: nil)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: Notification.Name.UIKeyboardWillShow, object: nil)
 
-        // Do any additional setup after loading the view.
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @objc func keyboardWillHide(notification:Notification) {
+        changeKeyBoardHeight(notification: notification)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @objc func keyboardWillShow(notification:Notification) {
+        changeKeyBoardHeight(notification: notification)
     }
-    */
-
+    
+    func changeKeyBoardHeight(notification:Notification) {
+        if let keyboardFrame = notification.userInfo?[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+            let keyHeight = keyboardFrame.cgRectValue.height
+            bottomConstraint.constant = keyHeight + 10
+        }
+    }
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+    }
+    
+    @IBAction func saveDateTapped(_ sender: UIButton) {
+    }
+    
+    @IBAction func calendarTapped(_ sender: UIButton) {
+    }
+    @IBAction func cameraTapped(_ sender: UIButton) {
+    }
+    
 }
